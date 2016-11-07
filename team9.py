cab2116fe1d5961c2e31fac27e2f9833514f1c89
Broberry <thomasmaina160@gmail.com>
2016-11-07 08:47:45 -0600

    this took too long

--- team9.py
+++ team9.py
@@ -6,8 +6,8 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Test' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+team_name = 'Awesmazing, IDK' # Only 10 chars displayed.
+strategy_name = 'GGez'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
